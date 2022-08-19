a02e9f6e01afef3606c51fbd656a0da5110486ea
Yauheni Buhaev <buhaev.yauheni@gmail.com>
2022-08-19 21:58:02 +0300

    Update script.js

--- mapty/script.js
+++ mapty/script.js
@@ -25,10 +25,23 @@ function position(pos) {
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  L.marker([latitude, longitude])
-    .addTo(map)
-    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-    .openPopup();
+  map.on("click", function (mapEvent) {
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("test")
+
+      .openPopup();
+  });
 }
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(position, errorFunction);
